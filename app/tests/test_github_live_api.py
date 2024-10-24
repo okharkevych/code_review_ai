526ca63b78985b346063dbe78b4f590f1ba480d7
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-24 20:22:02 +0300

    add test_fetch_github_repo_success_live() in test_github_live_api.py

--- app/tests/test_github_live_api.py
+++ app/tests/test_github_live_api.py
@@ -4,6 +4,8 @@ import httpx
 import pytest
 from dotenv import load_dotenv
 
+from app.main import fetch_github_repo
+
 load_dotenv()
 
 
@@ -26,3 +28,11 @@ async def test_check_github_token_validity():
 
     is_valid: bool = await check_github_token_validity()
     assert is_valid
+
+
+@pytest.mark.asyncio
+async def test_fetch_github_repo_success_live(github_repo_url):
+    repo_contents: list[dict] = await fetch_github_repo(github_repo_url)
+
+    assert len(repo_contents) == 15
+    assert repo_contents[0]['name'] == '.gitignore'

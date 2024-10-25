1ab4c5f2e6c98989ee32771f921c3b00b5ac760b
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-25 22:05:26 +0300

    update test_github_mocks_and_helpers.py based on the changes to GitHub functionality

--- app/tests/test_github_mocks_and_helpers.py
+++ app/tests/test_github_mocks_and_helpers.py
@@ -1,9 +1,9 @@
-from app.main import convert_to_api_url
+from app.github_api import convert_to_api_url
 
 
 def test_convert_to_api_url(github_repo_url):
     expected_url: str = (
-        'https://api.github.com/repos/okharkevych/alien_invasion/contents'
+        'https://api.github.com/repos/okharkevych-api-test/api_target/contents'
     )
     actual_url: str = convert_to_api_url(github_repo_url)
 

437baa0c6c9079dc9add85ec8c3b3af849a89749
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-25 21:55:51 +0300

    update perform_review() in open_ai_api.py based on the changes to GitHub and OpenAI functionality

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import os
 
+import httpx
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 from app.github_api import fetch_github_repo
-from app.open_ai_api import analyze_code
+from app.open_ai_api import process_gpt_prompt, create_code_review_prompt
 
 load_dotenv()
 
@@ -28,14 +29,18 @@ async def review_code(request: ReviewRequest) -> dict:
     return review_result
 
 
-async def perform_review(request: ReviewRequest) -> dict:
-    repo_contents: list[dict] = await fetch_github_repo(
+async def perform_review(request: ReviewRequest) -> str:
+    repo_contents: dict = await fetch_github_repo(
         request.github_repo_url
     )
-    review: dict = await analyze_code(
-        repo_contents,
-        request.assignment_description,
-        request.candidate_level
+    gpt_response: httpx.Response = await process_gpt_prompt(
+        gpt_prompt=create_code_review_prompt(
+            repo_contents,
+            request.assignment_description,
+            request.candidate_level
+        )
     )
+    gpt_response_json: dict = gpt_response.json()
+    review: str = gpt_response_json['choices'][0]['message']['content']
 
     return review

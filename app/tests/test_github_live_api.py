d54170f957b193cee0b71c20ccbb193a37cb99db
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-25 21:59:53 +0300

    update test_github_live_api.py based on the changes to GitHub functionality

--- app/tests/test_github_live_api.py
+++ app/tests/test_github_live_api.py
@@ -3,7 +3,7 @@ import os
 import httpx
 import pytest
 
-from app.main import fetch_github_repo
+from app.github_api import fetch_github_repo
 
 
 async def check_github_token_validity() -> bool:
@@ -29,8 +29,9 @@ async def test_github_token_is_valid():
 
 @pytest.mark.asyncio
 async def test_fetch_github_repo_success_live(github_repo_url):
-    repo_contents: list[dict] = await fetch_github_repo(github_repo_url)
+    repo_contents: dict = await fetch_github_repo(github_repo_url)
 
-    assert len(repo_contents) == 3
-    assert repo_contents[0]['name'] == '.gitignore'
-    assert repo_contents[2]['name'] == 'file_1'
+    assert len(repo_contents['file_structure']) == 3
+
+    assert repo_contents['file_structure'][0] == '.gitignore'
+    assert repo_contents['code_contents']['.gitignore'] == '.env\n'

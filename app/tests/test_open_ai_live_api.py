4c278c98cb265965facc17dd1bdae03c57adb9c7
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-25 22:01:02 +0300

    add test_process_gpt_prompt_reply_to_simple_prompt_received() in test_open_ai_live_api.py

--- app/tests/test_open_ai_live_api.py
+++ app/tests/test_open_ai_live_api.py
@@ -3,6 +3,8 @@ import os
 import httpx
 import pytest
 
+from app.main import process_gpt_prompt
+
 
 async def check_openai_key_validity() -> bool:
     openai_api_key: str = os.getenv('OPENAI_API_KEY')
@@ -23,3 +25,15 @@ async def test_openai_api_key_is_valid():
 
     is_valid: bool = await check_openai_key_validity()
     assert is_valid
+
+
+@pytest.mark.asyncio
+async def test_process_gpt_prompt_reply_to_simple_prompt_received():
+    response: httpx.Response = await process_gpt_prompt(
+        gpt_prompt='This is a test prompt. Just reply "Yes".'
+    )
+    response_json: dict = response.json()
+    gpt_reply_content: str = response_json['choices'][0]['message']['content']
+
+    assert response.status_code == 200
+    assert gpt_reply_content == 'Yes.'

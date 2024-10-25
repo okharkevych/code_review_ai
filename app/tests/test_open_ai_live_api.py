87d66854a6b30aeb51f66b79fcc014646cdb0cea
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-25 22:02:07 +0300

    add test_process_gpt_prompt_code_review_received() in test_open_ai_live_api.py

--- app/tests/test_open_ai_live_api.py
+++ app/tests/test_open_ai_live_api.py
@@ -3,7 +3,8 @@ import os
 import httpx
 import pytest
 
-from app.main import process_gpt_prompt
+from app.github_api import fetch_github_repo
+from app.main import process_gpt_prompt, create_code_review_prompt
 
 
 async def check_openai_key_validity() -> bool:
@@ -37,3 +38,24 @@ async def test_process_gpt_prompt_reply_to_simple_prompt_received():
 
     assert response.status_code == 200
     assert gpt_reply_content == 'Yes.'
+
+
+@pytest.mark.asyncio
+async def test_process_gpt_prompt_code_review_received(github_repo_url):
+    response: httpx.Response = await process_gpt_prompt(
+        gpt_prompt=create_code_review_prompt(
+            repo_contents=await fetch_github_repo(github_repo_url),
+            assignment_description='Create a script to greet user.',
+            candidate_level='Junior'
+        )
+    )
+    response_json: dict = response.json()
+    gpt_reply_content: str = response_json['choices'][0]['message']['content']
+    review_sections: list = [
+        'Found Files', 'Downsides/Comments', 'Rating', 'Conclusion'
+    ]
+
+    assert response.status_code == 200
+
+    for review_section in review_sections:
+        assert review_section in gpt_reply_content
